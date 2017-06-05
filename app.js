909bc772894437c658d3f8ccadc75c42970b7c1c
ryan <rpschill@gmail.com>
2017-06-04 21:20:53 -0400

    weekend updates

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 $( document ).ready( function () {
+
+    // Change style of navbar on scroll
+    window.onscroll = function () { myFunction() };
+    function myFunction() {
+        var navbar = document.getElementById( "myNavbar" );
+        var navItems = document.getElementsByClassName( "nav-item" );
+        if ( document.body.scrollTop > 100 || document.documentElement.scrollTop > 100 ) {
+            navbar.className = "w3-bar" + " w3-card-2" + " w3-animate-top" + " w3-white";
+            navIems.className = navItems.className.replace( " w3-hover-black w3-text-white", " w3-hover-white w3-text-black")
+        } else {
+            navbar.className = navbar.className.replace( " w3-card-2 w3-animate-top w3-white", "" );
+            navItems.className = navItems.className.replace( " w3-hover-white w3-text-black", " w3-hover-black w3-text-white");
+        }
+    }
     
     /**
      *
