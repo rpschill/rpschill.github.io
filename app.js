fbda2a3b9a5ed5810dabd6b472faf27f5530c05e
rpschill <rpschill@gmail.com>
2017-07-11 15:38:30 -0400

    add animations / update portfolio section

--- app.js
+++ app.js
@@ -48,4 +48,110 @@ $(function() {
         $( "#myNavbar" ).toggleClass( "w3-black" );
     } );
 
+    /**
+     *
+     * Smooth scrolling to target elements
+     *
+     */
+
+    // Select all links with hashes
+    $( 'a[href*="#"]' )
+        // Remove links that don't link to anything
+        .not( '[href="#"]' )
+        .not( '[href="#0"]' )
+        .click( function ( event ) {
+            // On-page links
+            if (
+                location.pathname.replace( /^\//, '' ) == this.pathname.replace( /^\//, '' )
+                &&
+                location.hostname == this.hostname
+            ) {
+                // Figure element to scroll to
+                var target = $( this.hash );
+                target = target.length ? target : $( '[name=' + this.hash.slice( 1 ) + ']' );
+                // Does a scroll target exist?
+                if ( target.length ) {
+                    // Only prevent default if animation is actually going to happen
+                    event.preventDefault();
+                    $( 'html, body' ).animate( {
+                        scrollTop: target.offset().top
+                    }, 800, function () {
+                        // Callback after animation
+                        // Must change focus for accessibility!!
+                        var $target = $( target );
+                        $target.focus();
+                        // Check if the target was focused
+                        if ( $target.is( ":focus" ) ) {
+                            return false;
+                        } else {
+                            // Add tabindex for elements that are not focusable
+                            $target.attr( 'tabindex', '-1' );
+                            // Set focus again
+                            $target.focus();
+                        }
+                    } );
+                }
+            }
+        } );
+    
+    /**
+     *
+     * Animate graphs when div enters viewport
+     *
+     */
+    
+    var $skills = $( '#skills' );
+    var $hGraph = $( '#hGraph95' );
+    var $jsGraph = $( '#jsGraph90' );
+    var $ngGraph = $( '#ngGraph85' );
+    var $pyGraph = $( '#pyGraph80' );
+
+    $hGraph.css( 'width', '0' );
+    $jsGraph.css( 'width', '0' );
+    $ngGraph.css( 'width', '0' );
+    $pyGraph.css( 'width', '0' );
+
+    $( window ).scroll( function () {
+        $hGraph.isInViewport().css( 'width', '95%' );
+        $jsGraph.isInViewport().css( 'width', '90%' );
+        $ngGraph.isInViewport().css( 'width', '85%' );
+        $pyGraph.isInViewport().css( 'width', '80%' );
+    } );
+
+    /**
+     *
+     *  Animate social media icons when div enters viewport
+     *
+     */
+
+    var $socialIcons = $( '#social-icons' );
+
+    $( window ).scroll( function () {
+        $socialIcons.isInViewport().addClass( 'animated fadeInUp' );
+    } );
+
+    /**
+     *
+     *  Animate About section when div enters viewport
+     *
+     */
+    
+    var $aboutContent = $( '#about-content' );
+
+    $( window ).scroll( function () {
+        $aboutContent.isInViewport().addClass( 'animated fadeInUp' );
+    } );
+
+    /**
+     *
+     *  Animate Portfolio section when div enters viewport
+     *
+     */
+
+    var $portfolioContent = $( '.portfolio-content' );
+
+    $( window ).scroll( function () {
+        $portfolioContent.isInViewport().addClass( 'animated fadeInUp' );
+    })
+
 } );
